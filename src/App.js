cbe37bbe120d4ca32b05388055f5fda8ae341449
“thesrsbuvttchi” <thesrinisharma@gmail.com>
2020-07-18 17:20:34 +0530

    ver 2 - added basic list elements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ToDo from './Components/toDoElement'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+      <ToDo />
+      <ToDo />
+      <ToDo />
+      <ToDo />
+      <ToDo />
+      <ToDo />
+      </ul>
     </div>
   );
 }
---
+++ src/Components/toDoElement.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ToDo extends React.Component{
+    render(){
+        return(
+            <li>
+                    <input type='checkbox'></input>
+                    <p style={{display : 'inline', margin : '3rem'}}>Placeholder</p>
+            </li>
+        );
+    }
+  } 
+  
+  
+export default ToDo;
