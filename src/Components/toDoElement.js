c31c72e155efba198b02ea68ad374ec6f47be67d
“thesrsbuvttchi” <thesrinisharma@gmail.com>
2020-07-20 15:38:00 +0530

    ...

--- src/Components/toDoElement.js
+++ src/Components/toDoElement.js
@@ -2,15 +2,24 @@ import React from 'react';
 
 class ToDo extends React.Component{
 
-    constructor()
-    {
+    constructor(){
         super();
         this.state= {style : {display : 'inline', margin : '1rem', textDecoration : 'none'}}
+        this.clickHandler = this.clickHandler.bind(this);
+    }
+
+    clickHandler(){
+        this.setState((PrevState)=>{
+            if(PrevState.style.textDecoration=='none')
+                return({style : {display : 'inline', margin : '1rem', textDecoration : 'line-through'}});
+            else
+                return({style : {display : 'inline', margin : '1rem', textDecoration : 'none'}});
+        });
     }
 
     render(){
         return(
-            <li>
+            <li onClick={this.clickHandler}>
                 <p style={this.state.style}>{this.props.message}</p>
             </li>
         );
