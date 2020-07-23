244c21a199c9fc271755141294238852295ea128
“thesrsbuvttchi” <thesrinisharma@gmail.com>
2020-07-23 19:52:03 +0530

    Updates

---
+++ precache-manifest.961509ef0be4944b088ed1a25a520ee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66fd2083b43b26955cbcb8f6f404547",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "d475de312d6f521ae367",
+    "url": "/app/static/css/main.16605c45.chunk.css"
+  },
+  {
+    "revision": "319de6ebd5013cca7c8c",
+    "url": "/app/static/js/2.38e3c2a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app/static/js/2.38e3c2a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d475de312d6f521ae367",
+    "url": "/app/static/js/main.51f0c18c.chunk.js"
+  },
+  {
+    "revision": "ba0288ce6e49fbb53781",
+    "url": "/app/static/js/runtime-main.a22863a4.js"
+  }
+]);
